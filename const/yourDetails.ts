a4333bcadf954325b7f7685c2e40f15a62118f06
Corbin <maplesyrupsucker@protonmail.com>
2023-09-21 02:42:07 +0700

    polygon contract
    contract

---
+++ const/yourDetails.ts
@@ -0,0 +1,7 @@
+// Replace this with your contract address
+export const myEditionDropContractAddress: string = "0x8D4cad4722BDCE4ef36C30c2Be57AEEc0B6B4048";
+
+// Replace this with your token id
+export const tokenId: string = "0";
+
+// loop through edition 0-3
